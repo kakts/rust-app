904282387245a9a56b3c18dc9eecfbab7a84d006
Keita Akutsu <kakts.git@gmail.com>
2018-04-13 01:11:18 +0900

    Delete target dir

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,7 +4,13 @@ fn main() {
     println!("Guess the number!");
     println!("Please input your guess.");
 
+    // let: create variables
+    // mut: let the variable mutable
+    // String::new() is a static method of String.
     let mut guess = String::new();
+
+    // read_line returns the value of Result type.
+    // Result type has expect() method.
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
     println!("You guessed: {}", guess);

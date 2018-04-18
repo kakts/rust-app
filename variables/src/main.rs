107ab66404cb7ed7d5613ef2d5bbe710e9ee96dd
Keita Akutsu <kakts.git@gmail.com>
2018-04-18 23:06:43 +0900

    Add statement sample

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,25 +1,12 @@
 fn main() {
-    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let x = 5;
 
-    let five_hundred = tup.0;
-    let six_point_four = tup.1;
-    let one = tup.2;
+    let y = {
+        let x = 3;
 
-    println!("1: {}, 2: {}, 3: {}", five_hundred, six_point_four, one);
+        // 式は終端にセミコロンをつけない つけると文になってしまう
+        x + 1
+    };
 
-    // array has same type of data.
-    let a = [1, 2, 3, 4, 5];
-
-    println!("1: {}, 2: {}", a[0], a[2]);
-
-    another_function();
-    add(2, 3);
-}
-
-fn another_function() {
-    println!("Another function.");
-}
-
-fn add(x: u32, y: u32) {
-    println!("{}", x + y);
+    println!("The value of y is: {}", y);
 }

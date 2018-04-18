666a0095f00277334989c7e310e0918d92d8dcdf
Keita Akutsu <kakts.git@gmail.com>
2018-04-18 23:01:36 +0900

    Add function sample

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -6,4 +6,20 @@ fn main() {
     let one = tup.2;
 
     println!("1: {}, 2: {}, 3: {}", five_hundred, six_point_four, one);
+
+    // array has same type of data.
+    let a = [1, 2, 3, 4, 5];
+
+    println!("1: {}, 2: {}", a[0], a[2]);
+
+    another_function();
+    add(2, 3);
+}
+
+fn another_function() {
+    println!("Another function.");
+}
+
+fn add(x: u32, y: u32) {
+    println!("{}", x + y);
 }

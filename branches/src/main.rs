ed9d584340b69662fe5e0b1af994d180fa795ca3
Keita Akutsu <kakts.git@gmail.com>
2018-04-18 23:33:04 +0900

    Array for loop

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,27 +1,9 @@
 
 use std::io;
 fn main() {
-    println!("Hello, world!");
+    let a = [10, 20, 30, 40, 50];
 
-    let condtion: bool = true;
-    let number = if condition {
-        5
-    } else {
-        "six" // error
-    };
-    println!("The value of number is: {}", number);
-
-    let mut input = String::new();
-    io::stdin().read_line(&mut input)
-        .expect("Failed to read line");
-
-    let input: u32 = input.trim().parse()
-        .expect("err");
-    if input < 5 {
-        println!("condition was true");
-    } else {
-        println!("condition was false");
+    for element in a.iter() {
+        println!("the value is: {}", element);
     }
-
-
 }

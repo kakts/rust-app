0cb931a1b58a4f0a66695de5872acc235207204d
Keita Akutsu <kakts.git@gmail.com>
2018-04-26 00:23:33 +0900

    Refactor rectangles.rs

--- structure/src/rectangles.rs
+++ structure/src/rectangles.rs
@@ -1,13 +1,26 @@
-fn main() {
-    let width1 = 30;
-    let height1 = 50;
-
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(width1, height1)
-    );
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn main() {
+    // Rectangle構造体のインスタンスを作る
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50
+    };
+
+    // rect1のポインタを渡すことでareaに借用する
+    // mainは所有権を保ってrect1を使用し続けれる
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(&rect1)
+    );
+
+
+    println!("{}", rect1.width);
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }

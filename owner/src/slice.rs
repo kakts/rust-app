7e0c2b8f5dacad34da77358b0cd293391d214d67
Keita Akutsu <kakts.git@gmail.com>
2018-04-25 23:13:24 +0900

    Add slice.rs

---
+++ owner/src/slice.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let my_string = String::from("hello world");
+
+    let word = first_word(&my_string[..]);
+
+    println!("{}", word);
+    //s.clear();
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i]
+        }
+    }
+
+    &s[..]
+}

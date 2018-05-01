9b7dac0ffbff94075a7e184b5e56b2c6e34fbf38
Keita Akutsu <kakts.git@gmail.com>
2018-05-01 23:31:01 +0900

    Add Quarter state

--- enum_sample/src/match.rs
+++ enum_sample/src/match.rs
@@ -1,19 +1,33 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    NewYork,
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 fn value_in_cents(coin: Coin) -> u32 {
     match coin {
-        Coin::Penny => 1,
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        },
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        },
     }
 }
 
 fn main() {
-    println!("Penny: {}", value_in_cents(Coin::Penny))
+    println!("Penny: {}", value_in_cents(Coin::Penny));
+    println!("Quarter: {}", value_in_cents(Coin::Quarter(UsState::Alaska)));
 }

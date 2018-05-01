641cdacc58334bbf984c095549769ac53ded67be
Keita Akutsu <kakts.git@gmail.com>
2018-05-01 23:56:05 +0900

    Add module sample

---
+++ communicator/src/lib.rs
@@ -0,0 +1,24 @@
+mod client;
+
+mod network {
+    fn connect() {
+
+    }
+
+    mod client {
+        fn connect() {
+
+        }
+    }
+}
+
+
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}

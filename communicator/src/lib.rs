a948d625cf1e3ffc2ecc8252394b081316dbad07
Keita Akutsu <kakts.git@gmail.com>
2018-05-02 23:54:28 +0900

    Refactor communicator.

--- communicator/src/lib.rs
+++ communicator/src/lib.rs
@@ -7,8 +7,14 @@ pub mod network;
 
 #[cfg(test)]
 mod tests {
+    // ここでは testsモジュール内になるので、一個階層を上げてclientにアクセスするには
+    // ::を先頭につける
+    // それか super::をつけるのでも良い
+    // useすることでtestsモジュール内にsuper::clientにアクセスできるようにする
+    use super::client
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+
+        client::connect();
     }
 }

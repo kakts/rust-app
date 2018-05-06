a2e393167b1f84500de16ce53ecf61decc48bed7
Keita Akutsu <kakts.git@gmail.com>
2018-05-07 00:20:01 +0900

    File open handling sample

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,3 +1,13 @@
+use std::fs::File;
+
 fn main() {
-    panic!("Crash and burn");
+    let f = File::open("hello.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => {
+            // ファイルを開く際に問題が会ったのでpanic
+            panic!("There was a problem opening the file: {:?}", error);
+        }
+    };
 }

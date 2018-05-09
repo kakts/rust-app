913e7cfbee2845a7ff7f862b1c1d12fbb6971162
Keita Akutsu <kakts.git@gmail.com>
2018-05-09 22:39:27 +0900

    Add generic struct method

--- generic/src/main.rs
+++ generic/src/main.rs
@@ -31,6 +31,12 @@ struct Point<T, U> {
     y: U,
 }
 
+impl<T, U> Point<T, U> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 
@@ -47,4 +53,6 @@ fn main() {
     let both_integer = Point {x:5, y: 10};
     let both_float = Point {x: 1.0, y: 4.0};
     let integer_and_float = Point {x: 5, y: 4.0};
+
+    println!("{}, {}, {}", both_integer.x, both_float.x, integer_and_float.x);
 }

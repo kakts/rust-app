dc0f83a32f7fdb3d8034d37beb5eae2746d81c4d
Keita Akutsu <kakts.git@gmail.com>
2018-05-15 23:38:23 +0900

    Add sample should_panic

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -10,6 +10,24 @@ impl Rectangle {
     }
 }
 
+pub struct Guess {
+    value: u32
+}
+
+impl Guess {
+    pub fn new(value: u32) -> Guess {
+        if value < 1 {
+            panic!("Guess value must be less than or equal to 1, got {}", value);
+        } else if value > 100 {
+            panic!("Guess value must be less than or equal to 100, got {}", value);
+        }
+
+        Guess {
+            value
+        }
+    }
+}
+
 pub fn add_two(a: i32) -> i32 {
     a + 2
 }
@@ -70,5 +88,11 @@ mod tests {
         assert!(result.contains("Carol"),
             "Greeting did not contain name, value was `{}`", result
         );
+    }
+
+    #[test]
+    #[should_panic(expected = "Guess value must be less than or equal to 100")]
+    fn greater_than_100() {
+        Guess::new(200);
     }
 }

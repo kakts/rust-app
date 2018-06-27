b6eb7a219b195dea1424380d16aa8d8cb99547af
Keita Akutsu <kakts.git@gmail.com>
2018-06-27 22:04:51 +0900

    Fix main.rs for stderr

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -13,7 +13,8 @@ fn main() {
     // Using unwrap_or_else, we can set the original Error handling.
     // if Ok, it returns the value which OK holds.
     // if Err, it calls code in the closure.
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(&args).unwrap_or_else(|_err| {
+       println!("Problem parsing arguments: {}", _err);
        process::exit(1); 
     });;
 
